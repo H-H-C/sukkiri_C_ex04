03935425ce737aebf04f34c5038bc1d16f77b69e
honghong <90856538+H-H-C@users.noreply.github.com>
2022-02-17 00:16:21 +0900

    4-1

---
+++ 4-1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(void)
+{
+
+  bool tenki = false;
+  if (tenki == true)
+  {
+    printf("洗濯をします\n");
+    printf("散歩しに行きます\n");
+  }
+  else
+  {
+    printf("映画を見ます\n");
+  }
+  return 0;
+}
