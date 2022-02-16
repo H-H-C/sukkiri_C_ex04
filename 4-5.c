68bbcd304071865a037ddfc0b4e1b5756ce49031
honghong <90856538+H-H-C@users.noreply.github.com>
2022-02-17 00:16:35 +0900

    4-5

---
+++ 4-5.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+typedef char String[1024];
+
+int main(void)
+{
+  String answerNo;
+  printf("かっこよくて最高な、C言語男子の名前は?\n");
+  printf("1:ミナト  2:ミサキ  3:ツバサ  ４:ミサエ>\n");
+  scanf("%s", answerNo);
+  int n = atoi(answerNo);
+  if (n == 2)
+  {
+    printf("大正解！\n");
+  }
+  else
+  {
+    printf("残念\n");
+  }
+
+  return 0;
+}
